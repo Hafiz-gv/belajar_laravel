5915da5ea0186719dc400b6ee7cf75f68af890cd
hafizpcr <hafiz24ti@mahasiswa.pcr.ac.id>
2025-09-25 09:41:07 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\MahasiswaController;
+
+Route::get('/mahasiswa', function () {
+    return 'Hallo Mahasiswa';
+});
+
+/**
+ * Display the specified resource.
+ */
+
+Route::get('/mahasiswa/{param1}',
+[MahasiswaController::class, 'show']);
+
+Route::get('/about', function () {
+    return view('halaman-about');
+});
+
+Route::get('/profil', function () {
+    return view('halaman-mahasiswa-profil');
+});
+
+Route::get('/detail', function () {
+    return view('halaman-mahasiswa-detail');
+});
+
+Route::get('/nim/{param1?}', function ($param1) {
+    return ('nim saya : '.$param1);
+});
