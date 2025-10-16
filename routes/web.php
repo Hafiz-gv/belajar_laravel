5f559ace72be8572a8bfc8860cba4d2d15e61651
hafizpcr <hafiz24ti@mahasiswa.pcr.ac.id>
2025-10-16 10:55:31 +0700

    pertemuan6

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
-
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\MahasiswaController;
 
@@ -36,3 +36,4 @@ Route::get('/nim/{param1?}', function ($param1) {
 Route::post('question/store', [QuestionController::class, 'store'])
 		->name('question.store');
 Route::get('/home', [HomeController::class, 'index']);
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
